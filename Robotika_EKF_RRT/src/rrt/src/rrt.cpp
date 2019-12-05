54b6c16b0c90182541ed271208dcec3f20aac9f4
sudiroeen <44321165+sudiroeen@users.noreply.github.com>
2019-12-06 03:18:38 +0700

    Update rrt.cpp

--- Robotika_EKF_RRT/src/rrt/src/rrt.cpp
+++ Robotika_EKF_RRT/src/rrt/src/rrt.cpp
@@ -142,7 +142,7 @@ PATH PathPlanning::RRT(const KONTUR& obstacle_, const Point2f& start_, const Poi
 			}
 		}
 		imshow(windowName, img);
-		waitKey(1);
+		waitKey(100);
 	}
 
 	PATH fixed_path;
@@ -154,51 +154,6 @@ PATH PathPlanning::RRT(const KONTUR& obstacle_, const Point2f& start_, const Poi
 	namedWindow("path_hasil", CV_WINDOW_NORMAL);
 	imshow("path_hasil", img_hasil);
 	return fixed_path;
-}
-
-// masih belum berhasil
-PATH PathPlanning::BezierSpline(PATH fixed_path_){
-	// -1.0000e+00, 0, 0
-	// -1.4901e-08, +5.2500e-01, 0
-	// +1.0000e+00, -5.0000e-01, 0
-	// +1.5000e+00, +4.5000e-01, 0
-	PATH smoothedPath;
-	fixed_path_.insert(fixed_path_.begin(), Point2f(fixed_path_[0].x+1, fixed_path_[0].y+1));
-	fixed_path_.insert(fixed_path_.end(), Point2f(fixed_path_[fixed_path_.size()-2].x+1, fixed_path_[fixed_path_.size()-2].y+1));
-	Mat M_Bezier = (Mat_<float>(4,4) << -1, 3, -3, 1,
-										 3, -6, 3, 0,
-										 -3, 3, 0, 0,
-										 1, 0, 0, 0);
-	float max_step_u = 15.0;
-	bool isAwal = true;
-	for(size_t s=1; s<fixed_path_.size()-2; s++){
-		Mat Posisi = (Mat_<float>(4,2) << fixed_path_[s-1].x, fixed_path_[s-1].y,
-										  fixed_path_[s].x, fixed_path_[s].y,
-										  fixed_path_[s+1].x, fixed_path_[s+1].y,
-										  fixed_path_[s+2].x, fixed_path_[s+2].y);
-		if(isAwal){
-			for(float step_u=0.0; step_u<=max_step_u; step_u+=1.0){
-				float u = step_u/max_step_u;
-				Mat seq_u = (Mat_<float>(1,4) << pow(u, 3.0), pow(u, 2.0), u, 1.0);
-				Mat hasil_ = seq_u * M_Bezier * Posisi;
-				smoothedPath.emplace_back(Point2f(hasil_.at<float>(0), hasil_.at<float>(1)));
-			}
-			isAwal = false;
-		}else{
-			for(float step_u=10.0; step_u<=max_step_u; step_u+=1.0){
-				float u = step_u/max_step_u;
-				Mat seq_u = (Mat_<float>(1,4) << pow(u, 3.0), pow(u, 2.0), u, 1.0);
-				Mat hasil_ = seq_u * M_Bezier * Posisi;
-				smoothedPath.emplace_back(Point2f(hasil_.at<float>(0), hasil_.at<float>(1)));
-			}
-		}
-	}
-
-	for(size_t r=1; r<smoothedPath.size(); r++){
-		cout << "smoothedPath: " << smoothedPath[r] << endl;
-		line(img_smooth, smoothedPath[r-1], smoothedPath[r], Scalar::all(0), 2);
-	}
-	return smoothedPath;
 }
 
 void PathPlanning::execute(){
@@ -235,7 +190,8 @@ void PathPlanning::execute(){
 	  for(size_t s=0; s<getPathRRT.size(); s++)
 	  	cout << getPathRRT[s] << endl;
 	  
-	  PATH smooth_path = BezierSpline(getPathRRT);
+	  PathSmoothing Smooth;	  
+	  PATH smooth_path = Smooth.BezierSpline(getPathRRT, img_smooth);
 
 	  namedWindow("[failed] Bezier Spline", CV_WINDOW_NORMAL);
 	  imshow("[failed] Bezier Spline", img_smooth);
